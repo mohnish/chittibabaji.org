1f5c10329336071025e66bbf27663cf685773f4f
Mohnish Thallavajhula <arrowgunz@gmail.com>
2012-02-23 19:39:25 -0800

    Added the missing images

--- galleryfoot.php
+++
@@ -1,4 +0,0 @@
-<br />
-<div style="text-align:center; font-size:9pt;">&copy; Copyright 2008 Sri Sadguru Chittibabaji Samsthanam. All Rights Reserved.</div>
-</body>
-</html>
--- main.php
+++
@@ -1,5 +0,0 @@
-welcome</legend>
-      <p><br />
-        The greatest revelation of mankind has been to those who have discovered themselves to be part of nature. A realisation that leads one towards the mysterious unknown and the unobvious. </p>
-        
-       <p> What is it? How does one get to know it? Fact remains that we can only infer speculate. We remain, the clueless. Wanderers, stuck to the notion of a path. But truth it seems is a pathless land. All those who are part of the supreme revelation in all ages, times and conditions are the mystics. Chittibaba was a mystic. This is about him.      </p>
